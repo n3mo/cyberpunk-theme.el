18f20245a1a7a5d811f201564da64a4dd87cc8dd
Nick Van Horn <nemo1211@gmail.com>
2012-09-27 19:16:15 -0400

    Cybertheme instructions and header info.

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 cyberpunk-theme.el
 ==================
 
-Cyberpunk theme for emacs built-in color theme support
+Cyberpunk theme for emacs built-in color theme support known loosely as <code>deftheme</code>. 
+
+Usage
+=====
+
+Ensure that cyberpunk-theme.el is on the custom-theme-load-path like this: 
+
+<pre>
+(add-to-list 'custom-theme-load-path "~/path/to/cyberpunk-theme.el")
+</pre>
+
+You can then load the theme at any time by running the command <code>M-x load-theme</code> and entering cyberpunk when prompted for "load custom theme: "
+
+If you want cyberpunk-theme to be set at startup, add the following line to your .emacs init file:
+
+<pre>
+(load-theme 'cyberpunk t)
+</pre>
+
+Acknowledgments 
+===============
+
+This theme was originally made available on the overtone/emacs-live github page (https://github.com/overtone/emacs-live) for use with the color-theme package. It was then ported to native theme support for emacs built-in color theme package known loosely as "deftheme" by ??? (original author has been lost to me... email me if you're the one deserving of credit!)
+
+The initial cyberpunk-theme lacked many mode-specific faces. This file strives to be as mode-specific as possible, with further tweaks that suit my fancy.
--- cyberpunk-theme.el
+++ cyberpunk-theme.el
@@ -1,16 +1,35 @@
-;; Cyberpunk Colour Theme
-;;
-;; "and he'd still see the matrix in his sleep, bright lattices of logic
-;; unfolding across that colorless void..."
-;; William Gibson, Neuromancer.
+;;; Cyberpunk Colour Theme
+
+;; Copyright 2012, Nick Van Horn
+
+;; Author: Nick Van Horn <nemo1211@gmail.com>
+;; Keywords: color theme cyberpunk
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 ;;
 ;; This theme was originally made available on the overtone/emacs-live
 ;; github page (https://github.com/overtone/emacs-live) for use with
 ;; the color-theme package. It was then ported to native theme support
-;; for emacs 24 by ???.
+;; for emacs built-in color theme package known loosely as "deftheme"
+;; by ??? (original author has been lost to me... email me if you're
+;; the one deserving of credit!)
 ;; 
-;; Additional mode-aware additions were added by
-;; https://github.com/n3mo 
+;; The initial cyberpunk-theme lacked many mode-specific faces. This
+;; file strives to be as mode-specific as possible, with further
+;; tweaks that suit my fancy.
 
 (deftheme cyberpunk
     "")
