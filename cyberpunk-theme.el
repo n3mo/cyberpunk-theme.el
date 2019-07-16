4c6ac252fc8cfbf7ab29e86e61ff9a5be56d736b
Sam Aaron <samaaron@gmail.com>
2019-07-16 16:50:46 +0100

    Add original author's name to provenance description
    Thanks so much for porting my theme to the newer deftheme format. 
    
    You've saved me a tonne of work :D

--- cyberpunk-theme.el
+++ cyberpunk-theme.el
@@ -29,10 +29,10 @@
 
 ;;; Commentary:
 
-;; This theme is a port of the overtone/emacs-live theme of the same name
-;; (https://github.com/overtone/emacs-live). The original theme was
-;; designed for use with the color-theme package. This theme adopts the
-;; new built-in theme support deftheme. Additionally, this
+;; This theme is a port of Sam Aaron's overtone/emacs-live theme of the 
+;; same name (https://github.com/overtone/emacs-live). The original theme 
+;; was designed for use with the color-theme package. This theme adopts 
+;; the new built-in theme support deftheme. Additionally, this
 ;; theme strives to offer as many mode-specific customizations as
 ;; possible, with further tweaks that suit my fancy.
 
